f7c760deff50c2ff639334aeaaa69651a9bd6d1b
HehNice <122663831+HehNiceArt@users.noreply.github.com>
2025-04-09 02:30:09 +0800

    clamp rep

--- Assets/Scripts/Core/ReputationManager.cs
+++ Assets/Scripts/Core/ReputationManager.cs
@@ -3,4 +3,9 @@ using UnityEngine;
 public class ReputationManager : MonoBehaviour
 {
     public int currentReputation = 50;
+
+    private void Update()
+    {
+        Mathf.Clamp(currentReputation, 0, 100);
+    }
 }
